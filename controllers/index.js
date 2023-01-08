8b6070ff457b83af3daa034229485cd3cb702b2e
Tigro <easterlingme95@gmail.com>
2023-01-07 19:17:02 -0600

    event creation page and function

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { Event } = require('../models');
 
 router.get('/', (req, res) => {
     try{
@@ -8,5 +9,25 @@ router.get('/', (req, res) => {
     }
 });
 
+router.get('/event', (req, res) => {
+    try{
+        res.render('event');
+    } catch(err){
+        res.status(500).json(err);
+    }
+});
+
+router.post('/event', async (req, res) => {
+    try {
+        const newEvent = await Event.create({
+            ...req.body,
+            user_id: req.session.user_id,
+        });
+
+        res.status(200).json(newEvent);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
 
 module.exports = router;
