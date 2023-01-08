d599b740131daefd9a78eba21ae2ec30ae72da0c
Kerry <kerryhokanson@gmail.com>
2023-01-08 15:44:40 -0600

    signup page adds user to db

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Event } = require('../models');
+const { User, Event } = require('../models');
 
 router.get('/', (req, res) => {
     try{
@@ -26,8 +26,29 @@ router.post('/event', async (req, res) => {
 
         res.status(200).json(newEvent);
     } catch (err) {
+        console.log(err)
         res.status(400).json(err);
     }
 });
+
+router.get('/signup', async (req, res) => {
+    try{
+        res.render('signup');
+    } catch(err){
+        res.status(500).json(err);
+    }
+});
+
+router.post('/signup', async (req, res) => {
+    try {
+      const { name, email, password } = req.body;
+      const user = await User.create({ name, email, password });
+      console.log(user);
+      res.render('/calendar');
+    } catch (error) {
+      console.error(error);
+      res.status(500).send(error.message);
+    }
+  });
 
 module.exports = router;
